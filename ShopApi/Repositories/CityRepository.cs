39f2d565ef7058d7b025aad0f92cd912cc87a71f
seryozhaharutyunyan <sharutyunyan7777@gmail.com>
2024-01-14 20:08:39 +0300

    1.0

---
+++ ShopApi/Repositories/CityRepository.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Models;
+using Repositories.Interfaces;
+
+namespace Repositories
+{
+    public class CityRepository : ICityRepository
+    {
+        private readonly ShopDb db;
+
+        public CityRepository(ShopDb db)
+        {
+            this.db = db;
+        }
+
+        public async Task<City?> CreateAsync(City data)
+        {
+            City City = (await db.Cities.AddAsync(data)).Entity;
+            return await db.SaveChangesAsync() == 1 ? City : null;
+        }
+
+        public async Task<bool?> DeleteAsync(int id)
+        {
+            City? city = db.Cities.Find(id);
+
+            if (city is null)
+            {
+                return null;
+            }
+
+            db.Cities.Remove(city);
+            return await db.SaveChangesAsync() == 1 ? true : false;
+        }
+
+        public async Task<IEnumerable<City>> RetrieveAllAsync()
+        {
+            IEnumerable<City> cities = db.Cities.ToList();
+            return await Task.FromResult<IEnumerable<City>>(cities);
+        }
+
+        public async Task<City?> RetrieveAsync(int id)
+        {
+            City? city = await db.Cities.FindAsync(id);
+            return city is null ? null : city;
+        }
+
+        public async Task<City?> UpdateAsync(int id, City data)
+        {
+            City? city = db.Cities.Find(id);
+
+            if (city is null)
+            {
+                return null;
+            }
+
+            data.CityId = id;
+            db.Cities.Update(data);
+
+            return await db.SaveChangesAsync() == 1 ? data : null;
+        }
+    }
+}
